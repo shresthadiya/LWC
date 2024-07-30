060a64643ae47a619a7091051ce747852b3bd771
Diya Shrestha <diyashrestha05@gmail.com>
2024-07-30 11:56:45 +0545

    parentcomp and pchildcomp LWC component added

---
+++ force-app/main/default/lwc/parentcomp/parentcomp.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class Parentcomp extends LightningElement {
+    value = 'This value is generated from parent LWC component';
+}
---
+++ force-app/main/default/lwc/pchildcomp/pchildcomp.js
@@ -0,0 +1,6 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Pchildcomp extends LightningElement {
+
+    @api getValueFromParent;
+}
