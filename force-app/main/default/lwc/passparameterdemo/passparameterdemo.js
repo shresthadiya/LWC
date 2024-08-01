d6644ab585bca6232352d67fe10b006912d33360
Diya Shrestha <diyashrestha05@gmail.com>
2024-08-01 15:51:39 +0545

    AccountSearClass Apex Class and passparameterdemo LWC Component added

---
+++ force-app/main/default/lwc/passparameterdemo/passparameterdemo.js
@@ -0,0 +1,30 @@
+import { LightningElement, wire } from 'lwc';
+import searchacc from '@salesforce/apex/AccountSearClass.searchAccounts';
+
+const columns = [
+    {label : 'Account Name', fieldName : 'Name'},
+    {label : 'Phone', fieldName : 'Phone'},
+    {label : 'Type', fieldName : 'Type'},
+    {label : 'Total Opportunity Amount', fieldName : 'Total_Opportunity_Amount__c'}
+]
+
+export default class Passparameterdemo extends LightningElement {
+    columns = columns;
+    accounts;
+    error;
+    searchkey = '';
+
+    handleonChange(event){
+        this.searchkey = event.target.value;
+    }
+
+    @wire(searchacc, {searchkey : '$searchKey'})
+    searchaccount({data, error}){
+        if(data){
+            this.accounts = data;
+        }
+        else{
+            this.error = error;
+        }
+    };
+}
