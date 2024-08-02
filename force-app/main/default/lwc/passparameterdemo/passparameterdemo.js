9d467b063a203d40ff095102c89b32dd8fc4f900
Diya Shrestha <diyashrestha2001@gmail.com>
2024-08-02 15:47:00 +0545

    Recent LWC file updated second

--- force-app/main/default/lwc/passparameterdemo/passparameterdemo.js
+++ force-app/main/default/lwc/passparameterdemo/passparameterdemo.js
@@ -1,30 +1,29 @@
-import { LightningElement, wire } from 'lwc';
-import searchacc from '@salesforce/apex/AccountSearClass.searchAccounts';
+import { api, LightningElement,track,wire} from 'lwc';
+import lstAccount from '@salesforce/apex/AccountSearClass.searchAcc';
 
 const columns = [
+
     {label : 'Account Name', fieldName : 'Name'},
     {label : 'Phone', fieldName : 'Phone'},
     {label : 'Type', fieldName : 'Type'},
-    {label : 'Total Opportunity Amount', fieldName : 'Total_Opportunity_Amount__c'}
-]
+    {label : 'Total Opportunity Amount', fieldName : 'Total_Opportunity_Amount__c'},
+];
 
-export default class Passparameterdemo extends LightningElement {
+
+export default class Testacademy extends LightningElement {
+    searchKey = '';
     columns = columns;
     accounts;
     error;
-    searchkey ='';
-
     handleonChange(event){
-        this.searchkey = event.target.value;
+        this.searchKey = event.target.value;
     }
-
-    @wire(searchacc, {searchkey :'$searchKey'})
-    searchaccount({data, error}){
-        if(data){
+    @wire(lstAccount,{searchKey : '$searchKey'})
+    wiredAccounts({ error, data }) {
+        if (data) {
             this.accounts = data;
-        }
-        else{
+        } else {
             this.error = error;
         }
-    };
+    }
 }
