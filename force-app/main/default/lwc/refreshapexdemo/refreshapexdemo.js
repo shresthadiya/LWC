75bbcaea94eaba479691356c65cd2382736ee052
Diya Shrestha <diyashrestha2001@gmail.com>
2024-08-05 14:07:44 +0545

    refreshapexdemo LWC Component added

---
+++ force-app/main/default/lwc/refreshapexdemo/refreshapexdemo.js
@@ -0,0 +1,46 @@
+import { LightningElement, wire } from 'lwc';
+import acc from '@salesforce/apex/GetAccountDetails.getAccount';
+import { deleteRecord } from 'lightning/uiRecordApi';
+import refreshApex from '@salesforce/apex';
+
+const cols = [
+    {label:'Account Name', fieldName:'Name'},
+    {label:'Phone', fieldName:'Phone'},
+    {label:'Type', fieldName: 'Type'},
+    {label : 'Total Opportunity Amount', fieldName : 'Total_Opportunity_Amount__c'}
+];
+
+export default class Refreshapexdemo extends LightningElement {
+    columns = cols;
+    accounts;
+    error;
+    selectedRecord;
+    wiredAccountList;
+
+    @wire(acc) wiredAccounts(result){
+        this.wiredAccountList = result;
+
+        if(result.data){
+            this.accounts = result.data;
+        }
+        else if(result.error){
+            this.error = result.error;
+        }
+    }
+
+    handleSelection(){
+        if(event.detail.selectedRows.length > 0){
+            this.selectedRecord = event.detail.selectedRows[0].Id;
+        }
+    }
+
+    handleDelete(){
+        deleteRecord(this.selectedRecord)
+        .then(() =>{
+            refreshApex(this.wiredAccountList);
+        })
+        .catch(error =>{
+
+        })
+    }
+}
